de2383fae6f40b1b7683bad6d0eddb87e57a657b
Mutiyah Muritala <mutiyah.kem@gmail.com>
2021-01-21 09:44:22 +0100

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,44 @@
+// var homeBtn = document.getElementById('home-btn');
+// var homePage = document.getElementById('home');
+
+// var createElectionBtn = document.getElementById('create-election-btn');
+// var createElectionPage = document.getElementById('create-election');
+
+// var candidateBtn = document.getElementById('candidate-btn');
+// var candidatePage = document.getElementById('candidate');
+
+
+// homeBtn.addEventListener('click', function(){
+//     // this.style.backgroundColor = "#fff";
+//     // this.style.color = "#b14b02";
+//     // this.style.padding = "10px 0 10px 12px";
+//     // this.style.width = "120%";
+//     // this.style.borderRadius = "20px";
+//     if(true){
+//         homePage.style.display = "block";
+//     }
+//      if(!true){
+//         homePage.style.display = "none";
+//     }
+// });
+
+// createElectionBtn.addEventListener('click', function(){
+//     if(true){
+//         createElectionPage.style.position = "relative";
+//         createElectionPage.style.display = "block";
+//     }
+//     if (!true){
+//         createElectionPage.style.display = "none";
+        
+//     }
+// })
+
+// candidateBtn.addEventListener('click', function(){
+//     if(true){
+//         candidatePage.style.display= "block";
+//     }
+//      if(!true){
+//         candidatePage.style.display= "none";
+
+//     }
+// })
